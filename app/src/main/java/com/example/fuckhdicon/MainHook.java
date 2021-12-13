11e3b28c00e266e0e31b10382c0141ca4e463ad0
方块君 <fkj@fkj233.cn>
2021-12-13 13:17:39 +0800

    init

--- app/src/main/java/com/example/fuckhdicon/MainHook.java
+++ app/src/main/java/com/example/fuckhdicon/MainHook.java
@@ -2,6 +2,7 @@ package com.example.fuckhdicon;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
 import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
@@ -14,8 +15,7 @@ public class MainHook implements IXposedHookLoadPackage {
             @Override
             protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
                 super.beforeHookedMethod(param);
-                param.args[6] = true;
-                param.setResult(param);
+                param.args[6] = false;
             }
         });
     }
