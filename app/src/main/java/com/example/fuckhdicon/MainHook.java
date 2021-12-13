7dd71488b4cbaefe740272a4e6cd7416dc88829c
方块君 <fkj@fkj233.cn>
2021-12-13 13:12:01 +0800

    init

---
+++ app/src/main/java/com/example/fuckhdicon/MainHook.java
@@ -0,0 +1,22 @@
+package com.example.fuckhdicon;
+
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedHelpers;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
+
+public class MainHook implements IXposedHookLoadPackage {
+
+    @Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!lpparam.packageName.equals("com.android.systemui")) return;
+        XposedHelpers.findAndHookConstructor("com.android.systemui.statusbar.policy.MobileSignalController$MiuiMobileState", lpparam.classLoader, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, boolean.class, String.class, int.class, int.class, int.class, int.class, new XC_MethodHook() {
+            @Override
+            protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
+                super.beforeHookedMethod(param);
+                param.args[6] = true;
+                param.setResult(param);
+            }
+        });
+    }
+}
